5fb1bdddddf9334956c0e1b6e50abd919a0a31e4
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-23 15:26:16 +0200

    form structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,35 @@ import './App.css'
 function App() {
   return (
     <>
-      {/* Pagina bianca */}
+
+      <div className="container my-4">
+        <h1>Nuovo Autore</h1>
+        <div className="card p-3">
+
+          <form className="row g-3 needs-validation" noValidate>
+            <div className="col-md-4">
+              <label htmlFor="validationCustom01" className="form-label">Author</label>
+              <input type="text" className="form-control" id="author" />
+              <div className="valid-feedback">
+                Looks good!
+              </div>
+            </div>
+            <div className="col-md-4">
+              <label htmlFor="validationCustom01" className="form-label">Title</label>
+              <input type="text" className="form-control" id="title" />
+              <div className="valid-feedback">
+                Looks good!
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">post's body</label>
+              <textarea className="form-control" name="body" id="body" rows="3"></textarea>
+            </div>
+
+
+          </form>
+        </div>
+      </div>
     </>
   )
 }
